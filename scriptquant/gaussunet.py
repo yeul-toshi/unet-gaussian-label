cb3f0b88a0890fd46f148aa70ddfba0a194741b2
yeul-toshi <yukiassassin19970330@gmail.com>
2022-03-23 18:55:40 +0900

    bug fixed

--- scriptquant/gaussunet.py
+++ scriptquant/gaussunet.py
@@ -13,8 +13,6 @@ from tensorflow.keras.layers import *
 from tensorflow.keras.activations import *
 from tensorflow.keras.utils import get_custom_objects
 
-
-# build model
 class Mish(Activation):
     def __init__(self, activation, **kwargs):
         super(Mish, self).__init__(activation, **kwargs)
@@ -37,33 +35,32 @@ def conv_bn(filters, x, act='relu'):
     conv = Activation(act)(conv)
     return conv
 
-def unet(input_size = (256, 256, 3)):
+def unet_bin(input_size = (256, 256, 3)):
     inputs = Input(input_size)
-    conv1 = conv_bn(64, inputs, act='Mish')
+    conv1 = conv_bn(32, inputs, act='Mish')
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = conv_bn(128, pool1, act='Mish')
+    conv2 = conv_bn(64, pool1, act='Mish')
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = conv_bn(256, pool2, act='Mish')
+    conv3 = conv_bn(128, pool2, act='Mish')
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
     
-    conv6 = conv_bn(512, pool3, act='Mish')
+    conv6 = conv_bn(256, pool3, act='Mish')
 
-    up7 = Conv2D(256, 2, activation = 'Mish', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2, 2))(conv6))
+    up7 = Conv2D(128, 2, activation = 'Mish', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2, 2))(conv6))
     merge7 = concatenate([conv3, up7], axis = 3)
-    conv7 = conv_bn(256, merge7, act='Mish')
-    up8 = Conv2D(128, 2, activation = 'Mish', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2, 2))(conv7))
+    conv7 = conv_bn(128, merge7, act='Mish')
+    up8 = Conv2D(64, 2, activation = 'Mish', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2, 2))(conv7))
     merge8 = concatenate([conv2, up8], axis = 3)
-    conv8 = conv_bn(128, merge8, act='Mish')
-    up9 = Conv2D(64, 2, activation = 'Mish', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2, 2))(conv8))
+    conv8 = conv_bn(64, merge8, act='Mish')
+    up9 = Conv2D(32, 2, activation = 'Mish', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2, 2))(conv8))
     merge9 = concatenate([conv1, up9], axis = 3)
-    conv9 = conv_bn(64, merge9, act='Mish')
-    conv10 = Conv2D(3, 1, activation = 'softmax')(conv9)
+    conv9 = conv_bn(32, merge9, act='Mish')
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
     model = Model(inputs = inputs, outputs = conv10)
 
     return model
 
-# scriptquant process
 loaded = False
 
 def initialize(inp: qc.InitializeInput, out: qc.InitializeOutput):
@@ -71,7 +68,7 @@ def initialize(inp: qc.InitializeInput, out: qc.InitializeOutput):
     out.processing.zoom = 1.0
     out.processing.tile_size = 608
     #out.ui.add_trackbar('value_tbr', 'Detection threshold (0~255):', 0, 255, 128, 10)
-    out.ui.add_textbox('value_txb', 'Detection threshold (0~255):', '60')
+    out.ui.add_textbox('value_txb', 'Detection threshold (0~255):', "60")
 
     out.ui.separated_into_rows = True
 
@@ -82,8 +79,8 @@ def initialize(inp: qc.InitializeInput, out: qc.InitializeOutput):
     global loaded, model, model_shape, model_input_size, infer
     if loaded == False:
         # Load tensorflow model
-        model = unet((608, 608, 3))
-        model.load_weights(inp.environment.app_path + '/scriptquant/unet.h5')
+        model = unet_bin((608, 608, 3))
+        model.load_weights(inp.environment.app_path + '/scriptquant/unet_bin.h5')
         # Get model's input shape
         model_shape = (1, *model.input_shape[1:])
         model_input_size = model.input_shape[2:0:-1]
@@ -98,8 +95,7 @@ def predict_for_block(block: np.ndarray):
     # Upscale image
     unet_pred = np.squeeze(unet_pred)
     unet_pred = np.array(unet_pred*255, dtype=np.uint8)
-    unet_pred = unet_pred[:, :, 0]
-    
+
     return unet_pred
 
 def process_tile(inp: qc.ProcessInput, out: qc.ProcessOutput):
